817d531287a000307b7090c687ec8acdb293c304
modox94 <modox94@yandex.ru>
2022-10-14 14:28:59 -0700

    WIP

--- cadence_sensor.py
+++ cadence_sensor.py
@@ -5,35 +5,27 @@ __author__ = 'Gus (Adapted from Adafruit)'
 __license__ = "GPL"
 __maintainer__ = "pimylifeup.com"
 
+count_of_magnets = 2
+pin_to_circuit = 7
+times_array = []
+
 GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pin_to_circuit, GPIO.IN)
 
 # define the pin that goes to the circuit
-pin_to_circuit = 7
 
 
-def rc_time(pin_to_circuit):
-    count = 0
-
-    # Output on the pin for
-    GPIO.setup(pin_to_circuit, GPIO.OUT)
-    GPIO.output(pin_to_circuit, GPIO.LOW)
+def check_sensor():
+    print(GPIO.input(pin_to_circuit))
     time.sleep(0.1)
-
-    # Change the pin back to input
-    GPIO.setup(pin_to_circuit, GPIO.IN)
-
-    # Count until the pin goes high
-    while (GPIO.input(pin_to_circuit) == GPIO.LOW):
-        count += 1
-
-    return count
+    return
 
 
 # Catch when script is interupted, cleanup correctly
 try:
     # Main loop
     while True:
-        print(rc_time(pin_to_circuit))
+        check_sensor()
 except KeyboardInterrupt:
     pass
 finally:
