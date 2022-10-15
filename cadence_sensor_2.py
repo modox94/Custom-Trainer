486692a99e4174ba492b14e4302b53454f5903b8
modox94 <modox94@yandex.ru>
2022-10-14 18:02:19 -0700

    WIP

---
+++ cadence_sensor_2.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+
+sensor_plus = 38
+sensor_ground = 40
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(sensor_plus, GPIO.OUT, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(sensor_ground, GPIO.IN)
+
+GPIO.output(sensor_plus, GPIO.HIGH)
+
+
+def my_callback(channel):
+    print('channel', channel)
+
+
+GPIO.add_event_detect(sensor_ground, GPIO.BOTH, callback=my_callback)
