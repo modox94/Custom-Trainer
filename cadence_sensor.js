1c6aa892e3fa38d1409ca5d65101b21f9731d5da
Nikita Novoselov <modox94@yandex.ru>
2022-10-16 16:38:09 -0700

    WIP

--- cadence_sensor.js
+++ cadence_sensor.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import { Gpio } from 'onoff';
 import Freq from 'frequency-counter';
 import { DIRECTION, EDGE, PHYSICAL_TO_GPIO } from './constants.js';
@@ -7,6 +8,16 @@ const sensorSignal = PHYSICAL_TO_GPIO[40];
 
 new Gpio(sensorPower, DIRECTION.high);
 const cadenceSignal = new Gpio(sensorSignal, DIRECTION.in, EDGE.rising);
+
+const dataFile = 'data.txt';
+
+try {
+  fs.unlinkSync(dataFile);
+} catch (error) {
+  console.log('unlink error', error);
+}
+
+fs.writeFileSync(dataFile, `Program launched: ${Date.now()}`, 'utf8');
 
 const window = 1;
 const resultWindow = 60;
@@ -20,6 +31,7 @@ const recordingSignals = (error, value) => {
     return;
   }
 
+  fs.appendFile(dataFile, '\n' + String(Date.now()));
   counter.inc(1);
 };
 
@@ -27,8 +39,12 @@ const calculateCadence = () => {
   const frequencyRaw = counter.freq();
   const frequency = frequencyRaw * (resultWindow / window / countMagnets);
 
-  console.log('frequencyRaw', frequencyRaw);
-  console.log('frequency', frequency);
+  const freqString = `frequencyRaw: ${frequencyRaw}
+  frequency: ${frequency}`;
+
+  fs.appendFile(dataFile, '\n' + freqString);
+
+  console.log(freqString);
 };
 
 cadenceSignal.watch(recordingSignals);
