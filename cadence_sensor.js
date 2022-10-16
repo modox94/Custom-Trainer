6b65320f91de9a2a049744bd219985522d74b150
Nikita Novoselov <modox94@yandex.ru>
2022-10-15 20:45:32 -0700

    WIP

--- cadence_sensor.js
+++ cadence_sensor.js
@@ -1,20 +1,22 @@
 import { Gpio } from 'onoff';
 import { DIRECTION, EDGE } from './constants.js';
 
-const sensorPower = 38;
-const sensorSignal = 40;
+// const sensorPower = 38;
+// const sensorSignal = 40;
+// const sensorPower = 38;
+const sensorSignal = 3;
 
-const cadencePower = new Gpio(sensorPower, DIRECTION.high);
+// const cadencePower = new Gpio(sensorPower, DIRECTION.high);
 // cadencePower.writeSync(Gpio.HIGH );
 const cadenceSignal = new Gpio(sensorSignal, DIRECTION.in, EDGE.both); // EDGE.rising);
 
-console.log('cadencePower', cadencePower.readSync());
+// console.log('cadencePower', cadencePower.readSync());
 console.log('cadenceSignal', cadenceSignal.readSync());
 
 console.log('-------------------');
 
 setInterval(() => {
-  console.log('cadencePower', cadencePower.readSync());
+  // console.log('cadencePower', cadencePower.readSync());
   console.log('cadenceSignal', cadenceSignal.readSync());
 }, 2000);
 
