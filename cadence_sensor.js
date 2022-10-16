e887a178dca1c42dd5b3d091646da1b5f36eeccd
Nikita Novoselov <modox94@yandex.ru>
2022-10-15 21:35:09 -0700

    WIP

--- cadence_sensor.js
+++ cadence_sensor.js
@@ -1,6 +1,21 @@
 import { Gpio } from 'onoff';
 import { DIRECTION, EDGE } from './constants.js';
+import rpio from 'rpio';
 
+const sensorPower = 38;
+const sensorSignal = 40;
+
+rpio.init({ mapping: 'gpio' });
+rpio.open(sensorPower, rpio.OUTPUT, rpio.LOW);
+rpio.open(sensorSignal, rpio.INPUT, rpio.PULL_DOWN);
+
+rpio.write(sensorPower, rpio.HIGH);
+
+setInterval(() => {
+  console.log('sensorSignal', rpio.read(sensorSignal));
+}, 2000);
+
+/*
 // const sensorPower = 38;
 // const sensorSignal = 40;
 
@@ -28,3 +43,4 @@ cadenceSignal.watch((error, value) => {
 
   console.log('value', value);
 });
+*/
