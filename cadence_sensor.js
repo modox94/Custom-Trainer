f3a7bb5e5ad6bab29baed2a6350cf2838e0861b5
Nikita Novoselov <modox94@yandex.ru>
2022-10-15 20:19:46 -0700

    WIP

--- cadence_sensor.js
+++ cadence_sensor.js
@@ -11,7 +11,7 @@ const cadenceSignal = new Gpio(sensorSignal, DIRECTION.in, EDGE.both); // EDGE.r
 console.log('cadencePower', cadencePower.readSync());
 console.log('cadenceSignal', cadenceSignal.readSync());
 
-setInterval(() => cadenceSignal.readSync(), 2000);
+setInterval(() => console.log('cadenceSignal', cadenceSignal.readSync()), 2000);
 
 cadenceSignal.watch((error, value) => {
   if (error) {
