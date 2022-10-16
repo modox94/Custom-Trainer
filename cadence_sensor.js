eb81e3dd31f50b372babfaa53f2dca7991a49e91
Nikita Novoselov <modox94@yandex.ru>
2022-10-15 22:24:40 -0700

    WIP

--- cadence_sensor.js
+++ cadence_sensor.js
@@ -1,42 +1,11 @@
 import { Gpio } from 'onoff';
-import { DIRECTION, EDGE } from './constants.js';
-import rpio from 'rpio';
+import { DIRECTION, EDGE, PHYSICAL_TO_GPIO } from './constants.js';
 
-/*
-const sensorPower = 38;
-const sensorSignal = 40;
+const sensorPower = PHYSICAL_TO_GPIO[38];
+const sensorSignal = PHYSICAL_TO_GPIO[40];
 
-rpio.init({ mapping: 'physical' });
-rpio.open(sensorPower, rpio.OUTPUT, rpio.LOW);
-// rpio.open(sensorSignal, rpio.INPUT, rpio.PULL_DOWN);
-rpio.open(sensorSignal, rpio.INPUT);
-
-rpio.write(sensorPower, rpio.HIGH);
-
-setInterval(() => {
-  console.log('sensorSignal', rpio.read(sensorSignal));
-}, 2000);
-*/
-
-// const sensorPower = 38;
-// const sensorSignal = 40;
-
-const sensorPower = 20;
-const sensorSignal = 21;
-
-const cadencePower = new Gpio(sensorPower, DIRECTION.high);
-// cadencePower.writeSync(Gpio.HIGH);
+new Gpio(sensorPower, DIRECTION.high);
 const cadenceSignal = new Gpio(sensorSignal, DIRECTION.in, EDGE.rising);
-
-console.log('cadencePower', cadencePower.readSync());
-console.log('cadenceSignal', cadenceSignal.readSync());
-
-console.log('-------------------');
-
-setInterval(() => {
-  console.log('cadencePower', cadencePower.readSync());
-  console.log('cadenceSignal', cadenceSignal.readSync());
-}, 2000);
 
 cadenceSignal.watch((error, value) => {
   if (error) {
