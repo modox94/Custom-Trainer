d4813a1dafb22b84da08b38d43adc308b570f11e
Nikita Novoselov <modox94@yandex.ru>
2022-10-15 23:18:09 -0700

    WIP

--- cadence_sensor.js
+++ cadence_sensor.js
@@ -1,4 +1,5 @@
 import { Gpio } from 'onoff';
+import Freq from 'frequency-counter';
 import { DIRECTION, EDGE, PHYSICAL_TO_GPIO } from './constants.js';
 
 const sensorPower = PHYSICAL_TO_GPIO[38];
@@ -7,10 +8,26 @@ const sensorSignal = PHYSICAL_TO_GPIO[40];
 new Gpio(sensorPower, DIRECTION.high);
 const cadenceSignal = new Gpio(sensorSignal, DIRECTION.in, EDGE.rising);
 
-cadenceSignal.watch((error, value) => {
+const timesArray = [];
+const maxTimeout = 5000;
+
+const counter = new Freq();
+
+const recordingSignals = (error, value) => {
   if (error) {
     throw error;
   }
 
+  // splice;
+  counter.inc();
   console.log('value', value);
-});
+};
+
+const calculateCadence = () => {
+  const res = counter.freq();
+  console.log('res', res);
+};
+
+cadenceSignal.watch(recordingSignals);
+
+setInterval(calculateCadence, 1000);
