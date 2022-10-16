39aab66e1231e4758f48d70e6c151cc483e43671
Nikita Novoselov <modox94@yandex.ru>
2022-10-15 19:56:25 -0700

    WIP

--- cadence_sensor.js
+++ cadence_sensor.js
@@ -4,10 +4,11 @@ import { DIRECTION, EDGE } from './constants.js';
 const sensorPower = 38;
 const sensorSignal = 40;
 
-new Gpio(sensorPower, DIRECTION.high);
+const cadencePower = new Gpio(sensorPower, DIRECTION.high);
 const cadenceSignal = new Gpio(sensorSignal, DIRECTION.in, EDGE.rising);
 
-console.log('check', cadenceSignal);
+console.log('cadencePower', cadencePower);
+console.log('cadenceSignal', cadenceSignal);
 
 cadenceSignal.watch((error, value) => {
   if (error) {
