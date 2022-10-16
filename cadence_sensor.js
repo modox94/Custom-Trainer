2f58e055d66ff770055b66d69307f5cfe1f9bbbf
Nikita Novoselov <modox94@yandex.ru>
2022-10-15 21:51:25 -0700

    WIP

--- cadence_sensor.js
+++ cadence_sensor.js
@@ -2,6 +2,7 @@ import { Gpio } from 'onoff';
 import { DIRECTION, EDGE } from './constants.js';
 import rpio from 'rpio';
 
+/*
 const sensorPower = 38;
 const sensorSignal = 40;
 
@@ -15,25 +16,25 @@ rpio.write(sensorPower, rpio.HIGH);
 setInterval(() => {
   console.log('sensorSignal', rpio.read(sensorSignal));
 }, 2000);
+*/
 
-/*
 // const sensorPower = 38;
 // const sensorSignal = 40;
 
-const sensorPower = 7;
-const sensorSignal = 8;
+const sensorPower = 20;
+const sensorSignal = 21;
 
-// const cadencePower = new Gpio(sensorPower, DIRECTION.high);
-// cadencePower.writeSync(Gpio.HIGH );
-const cadenceSignal = new Gpio(sensorSignal, DIRECTION.in, EDGE.both); // EDGE.rising);
+const cadencePower = new Gpio(sensorPower, DIRECTION.high);
+// cadencePower.writeSync(Gpio.HIGH);
+const cadenceSignal = new Gpio(sensorSignal, DIRECTION.in, EDGE.rising);
 
-// console.log('cadencePower', cadencePower.readSync());
+console.log('cadencePower', cadencePower.readSync());
 console.log('cadenceSignal', cadenceSignal.readSync());
 
 console.log('-------------------');
 
 setInterval(() => {
-  // console.log('cadencePower', cadencePower.readSync());
+  console.log('cadencePower', cadencePower.readSync());
   console.log('cadenceSignal', cadenceSignal.readSync());
 }, 2000);
 
@@ -44,4 +45,3 @@ cadenceSignal.watch((error, value) => {
 
   console.log('value', value);
 });
-*/
