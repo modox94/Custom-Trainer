05ab5020cf7961cf680e623d26c7a1517c7f46f0
Nikita Novoselov <modox94@yandex.ru>
2022-10-16 21:51:38 -0700

    WIP

---
+++ potentiometr_sensor_2.py
@@ -0,0 +1,13 @@
+import Encoder
+from time import sleep
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+Tuner = Encoder.Encoder(20, 21)
+
+
+
+while True:
+    print(Tuner.read())
+    sleep(.25)
