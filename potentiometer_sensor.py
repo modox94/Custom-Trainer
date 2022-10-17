8e2e2202e75f7c2df54d702d5f58c0cf457a3d24
Nikita Novoselov <modox94@yandex.ru>
2022-10-16 19:50:10 -0700

    potentiometer_sensor

---
+++ potentiometer_sensor.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+pin_a = 38
+pin_b = 40
+
+def discharge():
+    GPIO.setup(pin_a, GPIO.IN)
+    GPIO.setup(pin_b, GPIO.OUT)
+    GPIO.output(pin_b, False)
+    time.sleep(0.004)
+
+def charge_time():
+    GPIO.setup(pin_b, GPIO.IN)
+    GPIO.setup(pin_a, GPIO.OUT)
+    count = 0
+    GPIO.output(pin_a, True)
+    while not GPIO.input(b_pin):
+        count = count + 1
+    return count
+
+def analog_read():
+    discharge()
+    return charge_time()
+
+while True:
+    print(analog_read())
+    time.sleep(1)
