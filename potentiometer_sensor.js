9a0477005eff0e4b17d7f1dbafb9de93523316bc
Nikita Novoselov <modox94@yandex.ru>
2022-10-31 15:12:20 -0700

    WIP

--- potentiometer_sensor.js
+++ potentiometer_sensor.js
@@ -1,7 +1,18 @@
 const mcpadc = require("mcp-spi-adc");
 
-let potentiometerSensor;
-const tempVariable = mcpadc.open(5, err => {
+// let potentiometerSensor;
+// const tempVariable = mcpadc.open(5, err => {
+//   if (err) {
+//     console.log("err", err);
+//   }
+
+//   console.log("ptn", tempVariable);
+//   potentiometerSensor = tempVariable;
+// });
+
+// exports.potentiometerSensor = potentiometerSensor;
+
+exports.potentiometerSensor = mcpadc.open(5, err => {
   if (err) {
     console.log("err", err);
   }
@@ -9,5 +20,3 @@ const tempVariable = mcpadc.open(5, err => {
   console.log("ptn", tempVariable);
   potentiometerSensor = tempVariable;
 });
-
-exports.potentiometerSensor = potentiometerSensor;
