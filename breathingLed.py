130940cbb4fe98420853d48c293e679b154bead3
Nikita Novoselov <modox94@yandex.ru>
2022-11-07 14:41:21 -0800

    WIP

--- breathingLed.py
+++ breathingLed.py
@@ -1,28 +1,3 @@
-#              .';:cc;.
-#            .,',;lol::c.
-#            ;';lddddlclo
-#            lcloxxoddodxdool:,.
-#            cxdddxdodxdkOkkkkkkkd:.
-#          .ldxkkOOOOkkOO000Okkxkkkkx:.
-#        .lddxkkOkOOO0OOO0000Okxxxxkkkk:
-#       'ooddkkkxxkO0000KK00Okxdoodxkkkko
-#      .ooodxkkxxxOO000kkkO0KOxolooxkkxxkl
-#      lolodxkkxxkOx,.      .lkdolodkkxxxO.
-#      doloodxkkkOk           ....   .,cxO;
-#      ddoodddxkkkk:         ,oxxxkOdc'..o'
-#      :kdddxxxxd,  ,lolccldxxxkkOOOkkkko,
-#       lOkxkkk;  :xkkkkkkkkOOO000OOkkOOk.
-#        ;00Ok' 'O000OO0000000000OOOO0Od.
-#         .l0l.;OOO000000OOOOOO000000x,
-#            .'OKKKK00000000000000kc.
-#               .:ox0KKKKKKK0kdc,.
-#                      ...
-#
-# Author: peppe8o
-# Blog: https://peppe8o.com
-# Date: May 18th, 2021
-# Version: 1.0
-
 import RPi.GPIO as GPIO
 import sys
 import time
