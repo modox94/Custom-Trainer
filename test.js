017098cf43ababeae8b94d335bdd4b400aff8331
Nikita Novoselov <modox94@yandex.ru>
2022-12-07 12:30:47 -0800

    WIP test

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 const { motor } = require("./src/hardware/motor_driver");
 
 const fn = async () => {
+  console.log("start");
   const res1 = await motor.setLevel(4);
   console.log("res1", res1);
-
+  console.log("middle");
   const res2 = await motor.setLevel(8);
   console.log("res2", res2);
+  console.log("finish");
 };
 
 fn();
