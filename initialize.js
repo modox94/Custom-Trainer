f3c83a8a0aaa789edc1e2a3ec59efe898114e756
Nikita Novoselov <modox94@yandex.ru>
2023-02-19 23:23:11 -0800

    removed dead code

--- initialize.js
+++
@@ -1,9 +0,0 @@
-const MotorDriver = require("./src/hardware/motor_driver");
-
-const motor = new MotorDriver({
-  minPosition: 95,
-  maxPosition: 5,
-  sleepRatio: 1435,
-});
-
-motor.initialize();
